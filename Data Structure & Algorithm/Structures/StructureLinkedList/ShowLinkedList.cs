0d7302b5a04bb421002844b77a2f8803576f8daf
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-08-31 02:38:12 +0600

    show linkedList method class added

---
+++ Data Structure & Algorithm/Structures/StructureLinkedList/ShowLinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure___Algorithm.Structures.StructureLinkedList
+{
+    internal class ShowLinkedList
+    {
+        public static void ShowSingly(Node node)
+        {
+            Node currentNode = node;
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.data + " ");
+                currentNode = currentNode.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
