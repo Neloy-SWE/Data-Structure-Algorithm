0ea3368a35ea0d954ff531ab0c34d877c8158986
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-09-02 06:49:56 +0600

    doubly node added

---
+++ Data Structure & Algorithm/Structures/StructureLinkedList/Doubly/DoublyNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Data_Structure___Algorithm.Structures.StructureLinkedList.Doubly
+{
+    internal class DoublyNode
+    {
+        public int data;
+        public DoublyNode previous;
+        public DoublyNode next;
+
+        public DoublyNode(int data)
+        {
+            this.data = data;
+            this.previous = null!;
+            this.next = null!;
+        }
+    }
+}
