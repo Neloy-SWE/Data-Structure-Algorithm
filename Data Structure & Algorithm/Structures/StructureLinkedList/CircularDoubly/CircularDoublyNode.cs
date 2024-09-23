57df2005f8d9ef1731d4164daaf55fa4b3025df8
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2024-09-24 00:16:48 +0600

    with the last commit

--- Data Structure & Algorithm/Structures/StructureLinkedList/CircularDoubly/CircularDoublyNode.cs
+++ Data Structure & Algorithm/Structures/StructureLinkedList/CircularDoubly/CircularDoublyNode.cs
@@ -8,5 +8,15 @@ namespace Data_Structure___Algorithm.Structures.StructureLinkedList.CircularDoub
 {
     internal class CircularDoublyNode
     {
+        public CircularDoublyNode previous;
+        public int data;
+        public CircularDoublyNode next;
+
+        public CircularDoublyNode(int data)
+        {
+            this.data = data;
+            this.previous = null!;
+            this.next = null!;
+        }
     }
 }
