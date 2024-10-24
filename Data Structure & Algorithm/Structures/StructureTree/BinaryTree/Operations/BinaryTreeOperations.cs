4a0a5b1224b3d07c4c0c5291ed21754aff602138
Taufiq Ahommed Neloy <48958588+Neloy-SWE@users.noreply.github.com>
2024-10-25 01:49:17 +0600

    Binary tree search node method added

--- Data Structure & Algorithm/Structures/StructureTree/BinaryTree/Operations/BinaryTreeOperations.cs
+++ Data Structure & Algorithm/Structures/StructureTree/BinaryTree/Operations/BinaryTreeOperations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,6 +46,42 @@ namespace Data_Structure___Algorithm.Structures.StructureTree.BinaryTree.Operati
             Console.WriteLine("Insertion done!\n");
             BinaryTreeShow.Get(root);
             return root;
+        }
+
+        public static void SearchNode(BinaryTreeNode root, int data)
+        {
+            Console.WriteLine($"Searching node: {data}\n");
+
+            if (root == null)
+            {
+                Console.WriteLine("Data not found!\n\n\n");
+            }
+            else
+            {
+                BinaryTreeQueue queue = new();
+
+                queue.Enqueue(root);
+
+                while (queue.Count() > 0)
+                {
+                    BinaryTreeNode node = queue.Dequeue();
+
+                    if (node.data == data)
+                    {
+                        Console.WriteLine("Data found!\n\n\n");
+
+                        return;
+                    }
+
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                }
+
+                Console.WriteLine("Data not found!\n\n\n");
+            }
         }
     }
 }
